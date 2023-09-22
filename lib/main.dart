55f2a367eff64cf62c766884d9346dc9ba70cee2
apple <ferdoushwahid74@gmail.com>
2023-09-22 12:49:13 +0600

    assignment6

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Column(
-        children: [
-          Text('data'),
-          Text('asssignment6'),
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: const Text('My Shopping List '),
+        actions: [
+          Container(
+            padding: const EdgeInsets.only(right: 30),
+            width: 30,
+            child: const Icon(Icons.shopping_cart),
+          ),
+        ],
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.shopping_bag_sharp),
+            title: Text('Shopping Bag'),
+          ),
+          ListTile(
+            leading: Icon(Icons.apple),
+            title: Text('Buy Apple'),
+          ),
+          ListTile(
+            leading: Icon(Icons.music_note),
+            title: Text('Music Note'),
+          ),
+          ListTile(
+            leading: Icon(Icons.gamepad),
+            title: Text('Gaming Pad'),
+          ),
+          ListTile(
+            leading: Icon(Icons.mouse),
+            title: Text('Mouse'),
+          ),
+          ListTile(
+            leading: Icon(Icons.keyboard),
+            title: Text('Keyboard'),
+          ),
         ],
       ),
     );
